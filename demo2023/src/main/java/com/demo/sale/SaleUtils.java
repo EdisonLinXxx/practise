24cf6921c89cdec8ad10526737d8fc9fcf6ca041
edisonlin <542895045@qq.com>
2023-12-13 10:34:58 +0800

    优化例子

--- demo2023/src/main/java/com/demo/sale/SaleUtils.java
+++ demo2023/src/main/java/com/demo/sale/SaleUtils.java
@@ -1,15 +1,37 @@
 package com.demo.sale;
 
+import java.util.Optional;
+
 public class SaleUtils {
 
+    private static final String JX_SALE_TYPE_STR = "JX";
+    private static final Integer JX_SALE_TYPE_INT = 1;
+
+    //优化前
     public static Integer parseSaleType(String saleTypeStr){
         if(saleTypeStr == null || saleTypeStr.equals("")){
-          return null;
+            return null;
         }
         if(saleTypeStr.equals("JX")){
-          return 1;
+            return 1;
         }
         return null;
-      }
+    }
+
+    //优化1
+    public static Integer parseSaleType_New1(String saleTypeStr){
+
+        if(JX_SALE_TYPE_STR.equals(saleTypeStr)){
+            return JX_SALE_TYPE_INT;
+        }
+        return null;
+
+    }
+
+    //优化2
+    public static Integer parseSaleType_New2(String saleTypeStr){
+        return JX_SALE_TYPE_STR.equals(saleTypeStr) ? JX_SALE_TYPE_INT : null;
+        // return Optional.ofNullable(saleTypeStr).filter(JX_SALE_TYPE_STR::equals).map(o -> JX_SALE_TYPE_INT).orElse(null);
+    }
 
 }
